ad8bf4cbf02fdc210d8ca820db492574d50a4ea0
samir.rahimy <samir.rahimy@dexrasolutions.com>
2021-12-04 23:34:51 +0430

    added list recipe feature

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Tag, Ingredient
+from core.models import Tag, Ingredient, Recipe
 from recipe import serializers
 
 class BaseRecipeAttrViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
@@ -30,3 +30,16 @@ class IngredientViewSet(BaseRecipeAttrViewSet):
 
 	queryset = Ingredient.objects.all()
 	serializer_class = serializers.IngredientSerializer
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+	"""Manage recipes in the database"""
+
+	authentication_classes = (TokenAuthentication,)
+	permission_classes = (IsAuthenticated,)
+	serializer_class = serializers.RecipeSerializer
+	queryset = Recipe.objects.all()
+
+	def get_queryset(self):
+		"""Retrieve recipes for authenticated user"""
+		return self.queryset.filter(user = self.request.user)
